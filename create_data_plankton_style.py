86d0939eb07a5741c153155b4fc0137f462d98ba
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-12-10 22:02:30 +0100

    debugging

--- create_data_plankton_style.py
+++ create_data_plankton_style.py
@@ -1,16 +1,22 @@
 import os
+import glob
 
 import numpy as np
 import pandas as pd
 import skimage.io as sk
 
-
-
-def rgb2gray(rbg):
-    return np.dot(rbg[...,:3], [0.299, 0.587, 0.144])
-
-csv = pd.csv_read('./trans.csv')
+csv = pd.csv_read('./data/train.csv')
 whaleIDs = list(set(csv['whaleID']))
+train_paths = csv['Image']
+paths = glob.glob('./data/imgs/*')
+base_paths = [os.path.basename(p) for p in paths]
+test_paths = list(set(base_paths).difference(set(train_paths)))
+
+print "total path size: %s" % paths.shape
+print "size of train_paths %s" % train_paths.shape
+print "size of test_paths %s" % test_paths.shape
+
+assert False
 
 print "Making directories ..."
 for i in range(len(whaleIDs)):
@@ -18,8 +24,12 @@ for i in range(len(whaleIDs)):
     os.mkdir(path, 0777)
 
 print "Filling directories ..."
-for i in range(len(csv['Image'])):
-    path_from = "%s%s%s" (os.getcwd(), "/data/orig/train/", (csv['image'][i]))
-    path_to = "%s%s%s%s" (os.getcwd(), "/data/train/", (csv['whaleID'][i]), (csv['image'][i]))
+for i in range(len(train_paths)):
+    path_from = "%s%s%s" (os.getcwd(), "/data/imgs/", (train_paths[i]))
+    path_to = "%s%s%s%s" (os.getcwd(), "/data/train/", (csv['whaleID'][i]), (train_paths[i]))
     image = sk.imread(path_from)
     sk.imsave(path)
+
+
+for path in test_paths:
+    asd
