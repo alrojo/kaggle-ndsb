7caac51595a63d445e07faf15c2c6f60a913fb6b
Alexander Rosenberg Johansen <s145706@mnemosyne.imm.dtu.dk>
2015-12-11 07:24:15 +0100

    Data converter works! also cleaning up plankton files to make room for whales!

--- create_data_plankton_style.py
+++ create_data_plankton_style.py
@@ -1,35 +1,39 @@
 import os
 import glob
+import shutil
 
 import numpy as np
 import pandas as pd
 import skimage.io as sk
 
-csv = pd.csv_read('./data/train.csv')
+csv = pd.read_csv('./data/train.csv')
 whaleIDs = list(set(csv['whaleID']))
 train_paths = csv['Image']
 paths = glob.glob('./data/imgs/*')
 base_paths = [os.path.basename(p) for p in paths]
 test_paths = list(set(base_paths).difference(set(train_paths)))
 
-print "total path size: %s" % paths.shape
-print "size of train_paths %s" % train_paths.shape
-print "size of test_paths %s" % test_paths.shape
+print "total path size: %d" % len(paths)
+print "size of train_paths %d" % len(train_paths)
+print "size of test_paths %d" % len(test_paths)
+print "size haleIDs %d" % len(whaleIDs)
 
-assert False
+assert len(paths) == (len(train_paths) + len(test_paths))
+assert len(whaleIDs) == 447
 
-print "Making directories ..."
-for i in range(len(whaleIDs)):
-    path = "%s%s%s" (os.getcwd(), "/data/", whaleIDs[i])
-    os.mkdir(path, 0777)
+#print "Making directories ..."
+#for i in range(len(whaleIDs)):
+#    path = "%s%s%s" % (os.getcwd(), "/data/train/", whaleIDs[i])
+#    print path
+#    os.mkdir(path, 0777)
 
 print "Filling directories ..."
 for i in range(len(train_paths)):
-    path_from = "%s%s%s" (os.getcwd(), "/data/imgs/", (train_paths[i]))
-    path_to = "%s%s%s%s" (os.getcwd(), "/data/train/", (csv['whaleID'][i]), (train_paths[i]))
-    image = sk.imread(path_from)
-    sk.imsave(path)
-
+    path_from = "%s%s%s" % (os.getcwd(), "/data/imgs/", (train_paths[i]))
+    path_to = "%s%s%s%s%s" % (os.getcwd(), "/data/train/", (csv['whaleID'][i]), "/", (train_paths[i]))
+    shutil.move(path_from, path_to)
 
 for path in test_paths:
-    asd
+    path_from = "%s%s%s" % (os.getcwd(), "/data/imgs/", path)
+    path_to = "%s%s%s" % (os.getcwd(), "/data/test/", path)
+    shutil.move(path_from, path_to)
