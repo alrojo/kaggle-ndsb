1219f0517fa6beaecef9fc9d66601c3037f7a88b
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-12-17 15:25:48 +0100

    picplotter works, dataloader works

--- pic_plotter.py
+++ pic_plotter.py
@@ -1,11 +1,13 @@
 import numpy as np
-import utils
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-pics = utils.load_gz("test_time_pics.npy.gz")
+pics = np.load("chunk_x.npy")
 print pics.shape
-pic_list = [slice(pics, idx, axis=0) for idx in range(pics.shape[0])]
-plt.plot(pic_list[0])
+pic_list = []
+for i in range(pics.shape[0]):
+    pic_list.append(pics[i])
+plt.imshow(pic_list[641], cmap = cm.Greys_r)
 plt.plot(pic_list[20])
 plt.plot(pic_list[-1])
 plt.show
